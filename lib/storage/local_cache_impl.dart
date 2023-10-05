0419dd3e7e4f03fe959cbd6e1f6c3ce8839f754a
SokoMix <dimonsheu@mail.ru>
2023-10-05 23:56:06 +0300

    New changes

--- lib/storage/local_cache_impl.dart
+++ lib/storage/local_cache_impl.dart
@@ -1,5 +1,23 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
 import 'local_cache.dart';
 
 class SharedPrefsImpl implements SharedPrefs {
+  @override
+  Map<String, dynamic> getJson(String path) {
+    // TODO: implement getJson
+    throw UnimplementedError();
+  }
+
+  @override
+  Future init(SharedPreferences instance) {
+    // TODO: implement init
+    throw UnimplementedError();
+  }
+
+  @override
+  void saveJson(Map<String, dynamic> data) {
+    // TODO: implement saveJson
+  }
 
 }
